0242cec9132eb436c020861a0c1eaf45d418e4b2
Gaurav Paudel <gaurav.paudel@wesionary.team>
2023-06-09 17:08:25 +0545

    :sparkles: feat: added api gateway

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+  
+      // add lamba code
+      const myfunc = new lambda.Function(this, 'HelloHandler', {
+          runtime: lambda.Runtime.NODEJS_16_X,  
+          code: lambda.Code.fromAsset('./functions'),
+          handler: 'hello.handler'
+      });
+      
 
+    new apigateway.LambdaRestApi(this, 'LambdaProxy', {
+      handler: myfunc
+    });
   }
 }
